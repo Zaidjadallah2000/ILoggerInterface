9e7bb181c85316b0e8a3137e8150a28f1c2fe9bd
user <user@DESKTOP-493C7I8>
2025-08-11 13:54:27 +0300

    Add project files.

---
+++ ILoggerIterface/FileLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILoggerInterface
+{
+    internal class FileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FileLogger.txt");
+            var mess = $"{DateTime.Now} {message} {Environment.NewLine}";
+            File.AppendAllText(filePath, mess);
+        }
+    }
+}
---
+++ ILoggerIterface/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ILoggerInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger filelogger = new FileLogger();
+            filelogger.Log("This Is File Logger");
+            ILogger consolelogger = new ConsoleLogger();
+            consolelogger.Log("This Is Console Logger");
+
+        }
+
+       
+    }
+}
